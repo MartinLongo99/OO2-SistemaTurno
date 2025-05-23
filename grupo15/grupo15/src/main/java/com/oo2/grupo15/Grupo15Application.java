6daaf3710b6fa3c7d09d0df9acc827837343a354
MartinLongo99 <72109078+MartinLongo99@users.noreply.github.com>
2025-05-23 12:27:20 -0300

    Initial files of spring

---
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/Grupo15Application.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo15Application.class, args);
+	}
+
+}
---
+++ grupo15/grupo15/src/test/java/com/oo2/grupo15/Grupo15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
