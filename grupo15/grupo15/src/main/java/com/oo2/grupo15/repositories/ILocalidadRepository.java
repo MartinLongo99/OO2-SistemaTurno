984d661c7736788c45693dc20bf87ff4da1aaa62
MartinLongo99 <72109078+MartinLongo99@users.noreply.github.com>
2025-05-27 14:42:16 -0300

    Remove unused method

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/repositories/ILocalidadRepository.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/repositories/ILocalidadRepository.java
@@ -2,20 +2,14 @@ package com.oo2.grupo15.repositories;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oo2.grupo15.entities.Localidad;
 
 @Repository("localidadRepository")
 public interface ILocalidadRepository extends JpaRepository<Localidad, Serializable> {
-	Optional<Localidad> findByNombre(String nombre);
-
-    @Query("SELECT l FROM Localidad l JOIN FETCH l.provincia")
-    List<Localidad> findAllWithProvincia(); 
     
     List<Localidad> findByProvinciaIdOrderByNombreAsc(int provinciaId);
 }
