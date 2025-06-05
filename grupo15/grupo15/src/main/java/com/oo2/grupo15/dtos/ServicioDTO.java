4a885f2cb93fec802a311ebb1fe494f3347d8683
vSuprarg <lautaromourelle2@gmail.com>
2025-06-05 15:15:01 -0300

    Actualizacion Vista Servi

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/ServicioDTO.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/ServicioDTO.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -18,4 +22,18 @@ public class ServicioDTO {
     private boolean estado;
     private LocalTime horarioInicio;
     private LocalTime horarioFin;
+    private List<DayOfWeek> diasSemana;
+
+    // Método auxiliar para mostrar el horario completo en texto (por ejemplo "08:00 - 18:00")
+    public String getHorarioCompleto() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return horarioInicio.format(formatter) + " - " + horarioFin.format(formatter);
+    }
+
+    // Método auxiliar para mostrar los días de la semana como texto (por ejemplo "LUNES, MIERCOLES, VIERNES")
+    public String getDiasSemanaTexto() {
+        return diasSemana.stream()
+                .map(DayOfWeek::name)
+                .collect(Collectors.joining(", "));
+    }
 }
