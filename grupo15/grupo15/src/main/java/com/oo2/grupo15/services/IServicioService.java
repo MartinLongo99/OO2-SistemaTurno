861d1242f22974c477e817ea1ceec4c03e828a5f
MartinLongo99 <72109078+MartinLongo99@users.noreply.github.com>
2025-06-07 15:46:51 -0300

    Agrego filtros a vista de Servicios
    Se puede filtrar por nombre, estado o duracion

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/services/IServicioService.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/services/IServicioService.java
@@ -3,6 +3,7 @@ package com.oo2.grupo15.services;
 import java.util.List;
 
 import com.oo2.grupo15.dtos.ServicioDTO;
+import com.oo2.grupo15.entities.Servicio;
 
 public interface IServicioService {
     List<ServicioDTO> findAll();
@@ -11,4 +12,7 @@ public interface IServicioService {
     ServicioDTO update(Long id, ServicioDTO servicioDTO);
     void delete(Long id);
     List<ServicioDTO> getAll();
+    List<ServicioDTO> findByNombre(String nombre);
+	List<ServicioDTO> findByEstado(boolean estado);
+	List<ServicioDTO> findByDuracionMinutos(int duracion);
 }
