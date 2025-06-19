3c4d65a544ccfcfc8b20409753524dc20f1da16a
MartinLongo99 <72109078+MartinLongo99@users.noreply.github.com>
2025-06-19 18:30:42 -0300

    ExceptionHandler
    Se crea la clase GlobalExceptionHandler, que nos permite manejar las excepciones y quitar los try/catch del controller
    Esta clase va a almancenar todas las clases de tipo Exception

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/controllers/UsuarioController.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/controllers/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.oo2.grupo15.controllers;
 
-
 import com.oo2.grupo15.exceptions.DNIExistenteException;
 import com.oo2.grupo15.exceptions.UsuarioConTurnosActivosException;
 
@@ -23,95 +22,89 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-    @Autowired
-    private IUsuarioService usuarioService;
+	@Autowired
+	private IUsuarioService usuarioService;
 
-    @Autowired
-    private ITurnoService turnoService;
-    
-    @GetMapping
+	@Autowired
+	private ITurnoService turnoService;
+
+	@GetMapping
 	public String index(Model model) {
-    	model.addAttribute("usuarios", usuarioService.obtenerTodos());
-        model.addAttribute("usuarioNuevo", new UsuarioDTO());
+		model.addAttribute("usuarios", usuarioService.obtenerTodos());
+		model.addAttribute("usuarioNuevo", new UsuarioDTO());
 		return ViewRouteHelper.USUARIO_INDEX;
 	}
-    
-    @GetMapping("/todos")
-    public ModelAndView mostrarUsuarios() {
-        ModelAndView mAV = new ModelAndView(ViewRouteHelper.USUARIO_INDEX); 
-        mAV.addObject("usuarios", usuarioService.obtenerTodos());
-        mAV.addObject("usuarioNuevo", new UsuarioDTO());
-        return mAV;
-    }
-    
-    @GetMapping("/gestion")
-    public ModelAndView gestionarUsuarios(@RequestParam(name = "dni", required = false) Long dni) {
-        ModelAndView mAV = new ModelAndView(ViewRouteHelper.USUARIO_ALL); 
-        List<UsuarioDTO> usuarios;
 
-        if (dni != null) {
-            Optional<UsuarioDTO> usuarioEncontrado = usuarioService.findByContactoDni(dni);
-           
-            if (usuarioEncontrado.isPresent()) {
-                usuarios = List.of(usuarioEncontrado.get()); 
-            } else {
-                usuarios = List.of(); 
-            }
-        } else {
-            usuarios = usuarioService.getAll();
-        }
+	@GetMapping("/todos")
+	public ModelAndView mostrarUsuarios() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.USUARIO_INDEX);
+		mAV.addObject("usuarios", usuarioService.obtenerTodos());
+		mAV.addObject("usuarioNuevo", new UsuarioDTO());
+		return mAV;
+	}
 
-        mAV.addObject("usuarios", usuarios);
-        mAV.addObject("dniBuscado", dni); 
-        return mAV;
-    }
+	@GetMapping("/gestion")
+	public ModelAndView gestionarUsuarios(@RequestParam(name = "dni", required = false) Long dni) {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.USUARIO_ALL);
+		List<UsuarioDTO> usuarios;
 
-    @PostMapping("/guardar")
-    public ModelAndView guardarUsuario(@ModelAttribute("usuarioNuevo") UsuarioDTO dto, RedirectAttributes redirectAttributes) {
-        try {
-            if (dto.getId() == null) {
-                usuarioService.crearUsuario(dto);
-                redirectAttributes.addFlashAttribute("mensaje", "Usuario creado correctamente.");
-            } else {
-                usuarioService.actualizarUsuario(dto.getId(), dto);
-                redirectAttributes.addFlashAttribute("mensaje", "Usuario actualizado correctamente.");
-            }
-            return new ModelAndView("redirect:/usuarios");
-        } catch (DNIExistenteException e) {
-            redirectAttributes.addFlashAttribute("error", e.getMessage());
-            if (dto.getId() != null) {
-                return new ModelAndView("redirect:/usuarios/editar/" + dto.getId());
-            } else {
-                return new ModelAndView("redirect:/usuarios");
-            }
-        } 
-    }
+		if (dni != null) {
+			Optional<UsuarioDTO> usuarioEncontrado = usuarioService.findByContactoDni(dni);
 
-    @GetMapping("/eliminar/{id}")
-    public String eliminarUsuario(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-        try {
-            // Verificar si el usuario tiene turnos activos
-            if (turnoService.tieneTurnosActivos(id)) {
-                throw new UsuarioConTurnosActivosException(id);
-            }
-            
-            // Si no tiene turnos activos, proceder con la eliminación
-            usuarioService.eliminarUsuario(id);
-            redirectAttributes.addFlashAttribute("mensaje", "Usuario eliminado correctamente");
-            
-        } catch (UsuarioConTurnosActivosException ex) {
-            // Capturar la excepción y añadir un mensaje de error
-            redirectAttributes.addFlashAttribute("error", ex.getMessage());
-        }
-        
-        return "redirect:/usuarios";
-    }
+			if (usuarioEncontrado.isPresent()) {
+				usuarios = List.of(usuarioEncontrado.get());
+			} else {
+				usuarios = List.of();
+			}
+		} else {
+			usuarios = usuarioService.getAll();
+		}
 
-    @GetMapping("/editar/{id}")
-    public ModelAndView editarUsuario(@PathVariable Long id) {
-        UsuarioDTO usuario = usuarioService.obtenerPorId(id);
-        ModelAndView mAV = new ModelAndView("usuario/usuario-editar"); // Incluir la subcarpeta
-        mAV.addObject("usuarioNuevo", usuario);
-        return mAV;
-    }
+		mAV.addObject("usuarios", usuarios);
+		mAV.addObject("dniBuscado", dni);
+		return mAV;
+	}
+
+	@PostMapping("/guardar")
+	public ModelAndView guardarUsuario(@ModelAttribute("usuarioNuevo") UsuarioDTO dto,
+			RedirectAttributes redirectAttributes) {
+
+		if (dto.getId() == null) {
+			usuarioService.crearUsuario(dto);
+			redirectAttributes.addFlashAttribute("mensaje", "Usuario creado correctamente.");
+		} else {
+			usuarioService.actualizarUsuario(dto.getId(), dto);
+			redirectAttributes.addFlashAttribute("mensaje", "Usuario actualizado correctamente.");
+		}
+		return new ModelAndView(ViewRouteHelper.REDIRECT_USUARIOS_ROOT);
+
+	}
+
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUsuario(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+		try {
+			// Verificar si el usuario tiene turnos activos
+			if (turnoService.tieneTurnosActivos(id)) {
+				throw new UsuarioConTurnosActivosException(id);
+			}
+
+			// Si no tiene turnos activos, proceder con la eliminación
+			usuarioService.eliminarUsuario(id);
+			redirectAttributes.addFlashAttribute("mensaje", "Usuario eliminado correctamente");
+
+		} catch (UsuarioConTurnosActivosException ex) {
+			// Capturar la excepción y añadir un mensaje de error
+			redirectAttributes.addFlashAttribute("error", ex.getMessage());
+		}
+
+		return "redirect:/usuarios";
+	}
+
+	@GetMapping("/editar/{id}")
+	public ModelAndView editarUsuario(@PathVariable Long id) {
+		UsuarioDTO usuario = usuarioService.obtenerPorId(id);
+		ModelAndView mAV = new ModelAndView("usuario/usuario-editar"); // Incluir la subcarpeta
+		mAV.addObject("usuarioNuevo", usuario);
+		return mAV;
+	}
 }
