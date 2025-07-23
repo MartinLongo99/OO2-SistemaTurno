71fe2f7f993af7c2efbece44d77e37a03eb9fc7a
MartinLongo99 <72109078+MartinLongo99@users.noreply.github.com>
2025-07-23 18:06:17 -0300

    Record Class -Turno
    Se cambian las clases TurnoDTO y SolicitantoDTO a record Class
    Se modifica en el Controller la clase ModelMapper para que pueda utilizar Record en lugar de DTO

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/TurnoDTO.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/TurnoDTO.java
@@ -2,20 +2,11 @@ package com.oo2.grupo15.dtos;
 
 import java.time.LocalDateTime;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TurnoDTO {
-    private Long id;
-    private LocalDateTime fechaHora;
-    private boolean estado;
-    private Long servicioLugarId;
-    private Long solicitanteId;
-    
+public record TurnoDTO(
+    Long id,
+    LocalDateTime fechaHora,
+    boolean estado,
+    Long servicioLugarId,
+    Long solicitanteId
+) {
 }
