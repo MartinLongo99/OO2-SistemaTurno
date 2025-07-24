fcac049bcab91f83cd678507da187217337c4b8d
marki0 <markitosjr27@gmail.com>
2025-07-24 09:37:23 -0300

    Creo API de servicio y config de swagger

--- grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/ServicioDTO.java
+++ grupo15/grupo15/src/main/java/com/oo2/grupo15/dtos/ServicioDTO.java
@@ -1,61 +1,43 @@
-
 package com.oo2.grupo15.dtos;
 
 import java.time.DayOfWeek;
-import java.util.Set;
-import lombok.AllArgsConstructor;
-import java.time.LocalTime;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ServicioDTO {
-    private Long id;
-    private String nombre;
-    private Integer duracionMinutos;
-    private boolean estado;
-    private String horarioInicio;
-    private String horarioFin;
-    private Set<DayOfWeek> diasSemana; 
-	
-	public String getDiasSemanaFormateados() {
-		String resultadoFormateado;
+public record ServicioDTO(
+        Long id,
+        String nombre,
+        Integer duracionMinutos,
+        boolean estado,
+        String horarioInicio,
+        String horarioFin,
+        Set<DayOfWeek> diasSemana
+) {
+    public String getDiasSemanaFormateados() {
         if (diasSemana == null || diasSemana.isEmpty()) {
-        	resultadoFormateado = "N/A";
+            return "N/A";
         }
-
         List<DayOfWeek> sortedDays = diasSemana.stream()
-                                             .sorted(Comparator.comparing(DayOfWeek::getValue))
-                                             .collect(Collectors.toList());
+                .sorted(Comparator.comparing(DayOfWeek::getValue))
+                .collect(Collectors.toList());
 
-        resultadoFormateado = sortedDays.stream()
-                         .map(this::convertirDiaAES)
-                         .collect(Collectors.joining(", "));
-        
-        return resultadoFormateado;
+        return sortedDays.stream()
+                .map(this::convertirDiaAES)
+                .collect(Collectors.joining(", "));
     }
 
     private String convertirDiaAES(DayOfWeek dia) {
-        switch (dia) {
-            case MONDAY: return "Lunes";
-            case TUESDAY: return "Martes";
-            case WEDNESDAY: return "Miércoles";
-            case THURSDAY: return "Jueves";
-            case FRIDAY: return "Viernes";
-            case SATURDAY: return "Sábado";
-            case SUNDAY: return "Domingo";
-            default: return dia.toString(); 
-        }
+        return switch (dia) {
+            case MONDAY -> "Lunes";
+            case TUESDAY -> "Martes";
+            case WEDNESDAY -> "Miércoles";
+            case THURSDAY -> "Jueves";
+            case FRIDAY -> "Viernes";
+            case SATURDAY -> "Sábado";
+            case SUNDAY -> "Domingo";
+            default -> dia.toString();
+        };
     }
-    
-    
 }
